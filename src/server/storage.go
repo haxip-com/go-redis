90a8db757530e8739108c44d24ef74120556b32a
Morteza Mirzaei <mortezamirzai120@gmail.com>
2025-12-23 11:25:49 -0500

    Switch to RWMutex for storage.

--- src/server/storage.go
+++ src/server/storage.go
@@ -3,7 +3,7 @@ package main
 import "sync"
 
 type Store struct {
-	mu   sync.Mutex
+	mu   sync.RWMutex
 	data map[string][]byte
 }
 
@@ -12,8 +12,8 @@ func newStore() *Store {
 }
 
 func (s *Store) Get(key string) []byte {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+	s.mu.RLock()
+	defer s.mu.RUnlock()
 	return s.data[key]
 }
 
